f809ecc78dfc7a7ec85e6148357d8d0e55a5bf75
billydotexe <33807897+billydotexe@users.noreply.github.com>
2019-10-06 00:09:25 +0200

    Update script.js

--- sito/script.js
+++ sito/script.js
@@ -1,5 +1,15 @@
-function cambia(){
+function cambia()
+{
     document.getElementById("hamburger").classList.toggle("open");
     document.getElementById("header").classList.toggle("open");
     document.getElementById("links").classList.toggle("open");
+}
+
+function pwd()
+{
+    var flag = document.getElementById("showpwd");
+    var pwd = document.getElementById("password");
+    
+    if(flag.checked) pwd.setAttribute("type", "text");
+    else pwd.setAttribute("type", "password");
 }
